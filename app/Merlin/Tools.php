2ab727f604c7d9522a699b337ae99f4017d915a4
Joaquim Perez <jivkim@gmail.com>
2025-01-20 00:31:23 +0100

    feat(Merlin/Tools): convertTimestamp returns Carbon on null

--- app/Merlin/Tools.php
+++ app/Merlin/Tools.php
@@ -2,13 +2,15 @@
 
 namespace App\Merlin;
 
+use Carbon\Carbon;
+
 class Tools
 {
-    public static function convertTimestamp(int|string|null $merlinTimestamp): string
+    public static function convertTimestamp(?string $merlinTimestamp): ?Carbon
     {
-        if (!$merlinTimestamp) return "";
+        if (!$merlinTimestamp || $merlinTimestamp === '0') return null;
         $x = intval($merlinTimestamp);
         $ts = intval(6.3115e8 + 0.86402 * $x);
-        return date("d.m.Y H:i", $ts);
+        return new Carbon($ts);
     }
 }
