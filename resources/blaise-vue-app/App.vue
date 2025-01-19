a4d179ba5ad26e143eec79d140b2745b37199a42
Joaquim Perez <jivkim@gmail.com>
2025-01-19 18:10:00 +0100

    feat(App.vue): add search field

--- resources/blaise-vue-app/App.vue
+++ resources/blaise-vue-app/App.vue
@@ -1,8 +1,25 @@
 <script setup>
-import { Button } from "primevue";
+import { IconField, InputIcon, InputText } from "primevue";
 </script>
 
 <template>
-    <h1 class="text-xl text-primary">blaise</h1>
-    <Button>Go</Button>
+    <div class="md:max-w-screen-md md:mx-auto">
+        <h1 class="text-xl py-4">blaise</h1>
+        <IconField>
+            <InputIcon class="pi pi-search">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512"
+                    fill="currentColor"
+                    height="16"
+                    width="16"
+                >
+                    <path
+                        d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"
+                    />
+                </svg>
+            </InputIcon>
+            <InputText placeholder="Recherche client-e" />
+        </IconField>
+    </div>
 </template>
