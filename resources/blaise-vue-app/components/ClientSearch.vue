458e0a9ccbb565068365a7454a2b0baa8e36a8a2
Joaquim Perez <jivkim@gmail.com>
2025-01-19 19:53:56 +0100

    feat(ClientSearch): show search results

--- resources/blaise-vue-app/components/ClientSearch.vue
+++ resources/blaise-vue-app/components/ClientSearch.vue
@@ -1,10 +1,38 @@
 <script setup>
-import { IconField, InputIcon, InputText } from "primevue";
+import { Card, IconField, InputIcon, InputText } from "primevue";
+import { reactive } from "vue";
+
+const state = reactive({
+    query: "",
+    clients: [],
+});
+
+async function search() {
+    if (state.query.length < 2) {
+        state.clients = [];
+        return;
+    }
+    const response = await fetch(`/api/clients/search/${state.query}`);
+    const data = await response.json();
+    state.clients = data;
+}
 </script>
 
 <template>
     <IconField>
         <InputIcon class="pi pi-search" />
-        <InputText placeholder="Recherche client-e" />
+        <InputText
+            placeholder="Recherche client-e"
+            v-model="state.query"
+            @input="search"
+        />
     </IconField>
+
+    <div class="space-y-2 my-2">
+        <Card v-for="(client, index) in state.clients" :tabindex="index">
+            <template #content>
+                <div>{{ client.first_name }} {{ client.last_name }}</div>
+            </template>
+        </Card>
+    </div>
 </template>
