5ab8389e78932a042b65686842e0464614d63590
Joaquim Perez <jivkim@gmail.com>
2025-01-20 01:38:36 +0100

    log(AppServiceProvider): log db queries if DB_LOG_QUERIES=true

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Events\QueryExecuted;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (config("logging.log_db_queries")) {
+            DB::listen(function (QueryExecuted $query) {
+                $log = log10($query->time) + 1;
+                $magnitude = $log < 0 ? 0 : round($log);
+                $warnChars = str_pad(str_repeat(">", $magnitude), 5, " ");
+                $seconds = number_format($query->time / 1000, 3);
+                $sql = $query->toRawSql();
+                Log::channel("db")->info(
+                    "$warnChars ($seconds) | $sql"
+                );
+            });
+        }
     }
 }
