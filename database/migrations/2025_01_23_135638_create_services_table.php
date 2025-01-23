246ae0de6df317d418b0c3bd9982a000abe224c7
Joaquim Perez <jivkim@gmail.com>
2025-01-23 16:45:09 +0100

    new migration+model+controller for services table

---
+++ database/migrations/2025_01_23_135638_create_services_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\ServiceCategory;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignIdFor(ServiceCategory::class);
+            $table->integer('sort_order')->nullable();
+            $table->string('label')->nullable();
+            $table->decimal('price', 6, 2)->nullable();
+            $table->integer('execution_time')->nullable();
+            $table->integer('pause_time')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('services');
+    }
+};
