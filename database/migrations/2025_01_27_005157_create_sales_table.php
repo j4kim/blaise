8b3c19d01813b1d928386c74029cb4a1bcb9666b
Joaquim Perez <jivkim@gmail.com>
2025-01-27 01:57:03 +0100

    Sale model + migration + controller

---
+++ database/migrations/2025_01_27_005157_create_sales_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->decimal('base_price', 5, 2)->nullable();
+            $table->decimal('price_charged', 5, 2)->nullable();
+            $table->integer('quantity')->default(1);
+            $table->string('type')->nullable();
+            $table->string('label')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sales');
+    }
+};
