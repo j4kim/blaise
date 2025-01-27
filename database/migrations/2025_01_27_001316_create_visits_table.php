93f20cd63e482c46be7f0f3ce031ed836e69f31e
Joaquim Perez <jivkim@gmail.com>
2025-01-27 01:17:40 +0100

    create Visit model + migration + controller

---
+++ database/migrations/2025_01_27_001316_create_visits_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Client;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('visits', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignIdFor(Client::class);
+            $table->datetime('visit_date');
+            $table->decimal('billed', 5, 2)->nullable();
+            $table->text('notes')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('visits');
+    }
+};
