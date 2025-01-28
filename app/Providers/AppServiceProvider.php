b99f63f808c80df391e0058a3ecd6737a2254d6f
Joaquim Perez <jivkim@gmail.com>
2025-01-28 20:47:29 +0100

    AppServiceProvider: don't log select * from logs

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,11 +24,12 @@ class AppServiceProvider extends ServiceProvider
     {
         if (config("logging.log_db_queries")) {
             DB::listen(function (QueryExecuted $query) {
+                $sql = $query->toRawSql();
+                if (str_starts_with($sql, 'select * from "jobs"')) return;
                 $log = log10($query->time) + 1;
                 $magnitude = $log < 0 ? 0 : round($log);
                 $warnChars = str_pad(str_repeat(">", $magnitude), 5, " ");
                 $seconds = number_format($query->time / 1000, 3);
-                $sql = $query->toRawSql();
                 Log::channel("db")->info(
                     "$warnChars ($seconds) | $sql"
                 );
