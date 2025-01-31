250bc0f467ea81396fa5e9cb0a38b2fce5e254ea
Joaquim Perez <jivkim@gmail.com>
2025-01-31 01:52:07 +0100

    a new User view to check if user is well authenticated

--- resources/blaise-vue-app/router.js
+++ resources/blaise-vue-app/router.js
@@ -1,11 +1,13 @@
 import { createWebHashHistory, createRouter } from "vue-router";
 
 import Home from "./views/Home.vue";
+import User from "./views/User.vue";
 import Client from "./views/Client.vue";
 import Login from "./views/Login.vue";
 
 const routes = [
     { path: "/", component: Home, meta: { hideHeader: true } },
+    { path: "/user", component: User },
     {
         path: "/login",
         component: Login,
---
+++ resources/blaise-vue-app/views/User.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref } from "vue";
+import { get } from "../api";
+
+const user = ref({});
+
+get("/api/user").then(({ data }) => (user.value = data));
+</script>
+
+<template>
+    Vous êtes connecté·e en tant que {{ user.name }} ( {{ user.email }} )
+</template>
