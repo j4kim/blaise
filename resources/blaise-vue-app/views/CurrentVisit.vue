214b18bb55abe1315fec2e52ac5f0b1a5d93d818
Joaquim Perez <jivkim@gmail.com>
2025-02-02 01:19:25 +0100

    feat(CurrentVisit): show visit date

--- resources/blaise-vue-app/views/CurrentVisit.vue
+++ resources/blaise-vue-app/views/CurrentVisit.vue
@@ -1,12 +1,17 @@
 <script setup>
 import { Button } from "primevue";
 import { useVisitStore } from "../stores/visit";
+import dayjs from "dayjs";
 
 const visit = useVisitStore();
 </script>
 
 <template>
-    <h2 class="text-3xl font-extralight mb-3">Ticket en cours</h2>
+    <h2 class="text-3xl font-extralight mb-1">Ticket en cours</h2>
+    <h5 class="text-muted-color text-sm">
+        Visite du
+        {{ dayjs(visit.current.visit_date).format("DD.MM.YYYY HH:mm") }}
+    </h5>
 
     <div class="grow"></div>
 
