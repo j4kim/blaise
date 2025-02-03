4ab2ba20d2065eea29852bf1d95670ae065c19d6
Joaquim Perez <jivkim@gmail.com>
2025-02-03 01:21:48 +0100

    feat(Visit): relationship to client

--- app/Models/Visit.php
+++ app/Models/Visit.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Visit extends Model
@@ -11,6 +12,11 @@ class Visit extends Model
     public function sales(): HasMany
     {
         return $this->hasMany(Sale::class);
+    }
+
+    public function client(): BelongsTo
+    {
+        return $this->belongsTo(Client::class);
     }
 
     protected function total(): Attribute
