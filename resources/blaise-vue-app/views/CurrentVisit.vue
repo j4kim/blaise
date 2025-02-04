764a243133b8f9479f13cc6bb9e14deb92c44c7d
Joaquim Perez <jivkim@gmail.com>
2025-02-04 14:31:11 +0100

    feat(CurrentVisit): add notes on sale

--- resources/blaise-vue-app/views/CurrentVisit.vue
+++ resources/blaise-vue-app/views/CurrentVisit.vue
@@ -44,19 +44,22 @@ async function del() {
             <div
                 v-for="sale in visit.current.sales"
                 @click="visit.openSaleDialog(sale)"
-                class="flex justify-between text-xl items-center gap-2 cursor-pointer hover:bg-surface-200 dark:hover:bg-surface-800 px-5 py-3"
+                class="cursor-pointer hover:bg-surface-200 dark:hover:bg-surface-800 px-5 py-3"
             >
-                <div>{{ sale.label }}</div>
-                <div>
-                    CHF&nbsp;<span
-                        v-if="
-                            sale.base_price &&
-                            sale.price_charged != sale.base_price
-                        "
-                        class="line-through opacity-50"
-                        >{{ sale.base_price }}&nbsp;</span
-                    >{{ sale.price_charged ?? 0 }}
+                <div class="flex justify-between text-xl items-center gap-2">
+                    <div>{{ sale.label }}</div>
+                    <div>
+                        CHF&nbsp;<span
+                            v-if="
+                                sale.base_price &&
+                                sale.price_charged != sale.base_price
+                            "
+                            class="line-through opacity-50"
+                            >{{ sale.base_price }}&nbsp;</span
+                        >{{ sale.price_charged ?? 0 }}
+                    </div>
                 </div>
+                <div class="text-sm text-surface-500">{{ sale.notes }}</div>
             </div>
         </div>
 
