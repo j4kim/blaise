ad3186963361f92827ac6d8288b67d422696604f
Joaquim Perez <jivkim@gmail.com>
2025-02-25 18:07:52 +0100

    feat(Articles): list articles in a data table

--- resources/blaise-vue-app/views/admin/Articles.vue
+++ resources/blaise-vue-app/views/admin/Articles.vue
@@ -1,5 +1,30 @@
-<script setup></script>
+<script setup>
+import { Column, DataTable } from "primevue";
+import { useArticlesStore } from "../../stores/articles";
+
+const store = useArticlesStore();
+</script>
 
 <template>
-    <h2 class="text-3xl font-extralight mt-4">Articles</h2>
+    <div class="flex flex-col h-full">
+        <header class="py-2 px-3 flex gap-3 justify-between flex-wrap">
+            <span class="text-xl font-extralight">Articles</span>
+        </header>
+
+        <DataTable
+            :value="store.articles"
+            paginator
+            :rows="15"
+            paginatorTemplate="CurrentPageReport FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink"
+            currentPageReportTemplate="articles {first} à {last} sur {totalRecords}"
+        >
+            <Column field="sort_order" header="Ordre"></Column>
+            <Column field="barcode" header="Code barre"></Column>
+            <Column field="label" header="Nom"></Column>
+            <Column field="brand.name" header="Marque"></Column>
+            <Column field="line.name" header="Gamme"></Column>
+            <Column field="catalog_price" header="Prix catalogue"></Column>
+            <Column field="retail_price" header="Prix"></Column>
+        </DataTable>
+    </div>
 </template>
