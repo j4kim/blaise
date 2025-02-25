393ac43880aa7d47a212061406744c0a04ede69d
Joaquim Perez <jivkim@gmail.com>
2025-02-25 18:39:38 +0100

    feat(Articles): show creation date

--- resources/blaise-vue-app/views/admin/Articles.vue
+++ resources/blaise-vue-app/views/admin/Articles.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { Column, DataTable } from "primevue";
 import { useArticlesStore } from "../../stores/articles";
+import { formatDate } from "../../tools";
 
 const store = useArticlesStore();
 </script>
@@ -14,22 +15,28 @@ const store = useArticlesStore();
         <DataTable
             :value="store.articles"
             paginator
-            :rows="15"
+            :rows="10"
             paginatorTemplate="CurrentPageReport FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink"
             currentPageReportTemplate="articles {first} à {last} sur {totalRecords}"
             sortField="sort_order"
             :sortOrder="1"
         >
+            <Column
+                field="created_at"
+                header="Création"
+                bodyClass="tabular-nums"
+                sortable
+            >
+                <template #body="slotProps">
+                    {{ formatDate(slotProps.data.created_at) }}
+                </template>
+            </Column>
             <Column field="sort_order" header="Ordre" sortable></Column>
             <Column field="barcode" header="Code barre" sortable></Column>
             <Column field="label" header="Nom" sortable></Column>
             <Column field="brand.name" header="Marque" sortable></Column>
             <Column field="line.name" header="Gamme" sortable></Column>
-            <Column
-                field="catalog_price"
-                header="Prix catalogue"
-                sortable
-            ></Column>
+            <Column field="catalog_price" header="Prix cat." sortable></Column>
             <Column field="retail_price" header="Prix" sortable></Column>
         </DataTable>
     </div>
