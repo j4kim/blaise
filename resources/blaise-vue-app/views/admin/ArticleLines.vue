a1eb0c9eff28e7673d3268cceb8bde42dd8c49bd
Joaquim Perez <jivkim@gmail.com>
2025-02-25 23:31:51 +0100

    feat(ArticleLines): filter

--- resources/blaise-vue-app/views/admin/ArticleLines.vue
+++ resources/blaise-vue-app/views/admin/ArticleLines.vue
@@ -1,9 +1,12 @@
 <script setup>
-import { Column, DataTable } from "primevue";
+import { Column, DataTable, IconField, InputIcon, InputText } from "primevue";
 import { useArticlesStore } from "../../stores/articles";
 import { formatDate } from "../../tools";
+import { ref } from "vue";
 
 const store = useArticlesStore();
+
+const search = ref("");
 </script>
 
 <template>
@@ -17,7 +20,29 @@ const store = useArticlesStore();
             :alwaysShowPaginator="false"
             sortField="name"
             :sortOrder="1"
+            :filters="{
+                global: {
+                    value: search,
+                },
+            }"
+            :globalFilterFields="['name']"
         >
+            <template #header>
+                <div class="flex justify-end gap-3">
+                    <IconField>
+                        <InputIcon>
+                            <i class="pi pi-search" />
+                        </InputIcon>
+                        <InputText
+                            v-model="search"
+                            placeholder="Filtrer"
+                            size="small"
+                            variant="filled"
+                        />
+                    </IconField>
+                </div>
+            </template>
+            <template #empty> Aucune marque trouvée </template>
             <Column
                 field="created_at"
                 header="Création"
