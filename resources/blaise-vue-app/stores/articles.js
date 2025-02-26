22804a8b49bbaed2739864908856ca5c248fd216
Joaquim Perez <jivkim@gmail.com>
2025-02-26 12:12:01 +0100

    Revert "Revert "refactor(articles): split fetch functions to reload only articles""
    This reverts commit 1f4ccb99f905bb8eceb621c8b534db91fa02ba12.

--- resources/blaise-vue-app/stores/articles.js
+++ resources/blaise-vue-app/stores/articles.js
@@ -42,23 +42,30 @@ export const useArticlesStore = defineStore("articles", {
                 .join(" ");
             return article;
         },
+        async fetchArticles() {
+            const { response, data } = await get("/api/articles");
+            if (response.ok) {
+                this.articles = data.map((a) => this.prepareArticle(a));
+            }
+        },
+        async fetchBrands() {
+            const { response, data } = await get("/api/admin/articles/brands");
+            if (response.ok) {
+                this.brands = data;
+            }
+        },
+        async fetchLines() {
+            const { response, data } = await get("/api/admin/articles/lines");
+            if (response.ok) {
+                this.lines = data;
+            }
+        },
         async fetch() {
-            const [articles, brands, lines] = await Promise.all([
-                get("/api/articles"),
-                get("/api/admin/articles/brands"),
-                get("/api/admin/articles/lines"),
+            await Promise.all([
+                this.fetchArticles(),
+                this.fetchBrands(),
+                this.fetchLines(),
             ]);
-            if (articles.response.ok) {
-                this.articles = articles.data.map((a) =>
-                    this.prepareArticle(a)
-                );
-            }
-            if (brands.response.ok) {
-                this.brands = brands.data;
-            }
-            if (lines.response.ok) {
-                this.lines = lines.data;
-            }
         },
         openArticleEditDialog(article) {
             this.articleDialogHeader = "Modifier l'article";
@@ -88,7 +95,7 @@ export const useArticlesStore = defineStore("articles", {
                 : this.createArticle(attrs);
             const { data, response } = await promise;
             if (!response.ok) return;
-            await this.fetch();
+            await this.fetchArticles();
             this.showArticleDialog = false;
         },
         async updateArticle(id, attrs) {
@@ -100,7 +107,7 @@ export const useArticlesStore = defineStore("articles", {
         async deleteArticle(id) {
             const { data, response } = await del(`/api/admin/articles/${id}`);
             if (!response.ok) return;
-            await this.fetch();
+            await this.fetchArticles();
         },
         async deleteBrand(id) {
             const { data, response } = await del(
